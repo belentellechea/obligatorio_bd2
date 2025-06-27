abfda9ac9e3b8c48c0f9d1c7d82ef5965ec5c522
JBevc <141636441+JBevc@users.noreply.github.com>
2025-06-27 09:32:48 -0300

    arregle agregue list component a voteParty

--- frontend/src/pages/VoteParty/index.jsx
+++ frontend/src/pages/VoteParty/index.jsx
@@ -1,31 +1,32 @@
 import ListComponent from "../../components/ListComponent";
+import ListContainer from "../../components/ListContainer";
 import "./VoteParty.css";
 import { useNavigate } from "react-router-dom";
 
 const data = [
   {
-    candidato: "Yamandú Orsi",
+    title: "Yamandú Orsi",
     partido: "partido Frente Amplio",
     image: "./src/assets/partidos/Logo_Frente_Amplio.png",
   },
   {
-    candidato: "Alvaro Delgado",
+    title: "Alvaro Delgado",
     partido: "partido Nacional",
     image: "./src/assets/partidos/Flag_of_the_National_Party_(Uruguay).png",
   },
   {
-    candidato: "Andrés Ojeda",
+    title: "Andrés Ojeda",
     partido: "partido Colorado",
     image: "./src/assets/partidos/Flag_of_Colorado_Party_(Uruguay).png",
   },
   {
-    candidato: "Manini Ríos",
+    title: "Manini Ríos",
     partido: "partido Cablido Abierto",
     image:
       "./src/assets/partidos/Partido_cabildo_abierto_270x180_flag_version.jpg",
   },
   {
-    candidato: "Pablo Mieres",
+    title: "Pablo Mieres",
     partido: "partido Independiente",
     image: "./src/assets/partidos/Bandera_Partido_Independiente.png",
   },
@@ -38,13 +39,7 @@ export default function VoteParty() {
     <div className="container VoteParty">
       <h1> Elige un partido </h1>
       <div className="partyList">
-        {data.map((element, index) => (
-          <ListComponent
-            key={index}
-            title={element.partido}
-            imageUrl={element.image}
-          />
-        ))}
+        <ListContainer data={data} />
       </div>
 
       <button className="cancelButton" onClick={() => navigate("/home")}>
