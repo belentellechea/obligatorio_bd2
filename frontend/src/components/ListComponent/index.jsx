c5507622d74170c4b30ddae6a7ba1146de7646ce
belentellechea <141636481+belentellechea@users.noreply.github.com>
2025-07-06 21:50:43 -0300

    Create index.jsx

---
+++ frontend/src/components/ListComponent/index.jsx
@@ -0,0 +1,14 @@
+import "./ListComponent.css";
+
+export default function ListComponent({ imageUrl, title, onClick, image }) {
+  return (
+    <div className="listComponentContainer" onClick={onClick}>
+      <div className="listImageContainer">
+        <img src={imageUrl} className="listImage"></img>
+      </div>
+      <div className="titleContainer">
+        <p className="listTitle"> {title} </p>
+      </div>
+    </div>
+  );
+}
